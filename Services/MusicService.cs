5e470f328c6ecf9d539aa53bb84cbb3b67202953
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2025-08-16 17:50:07 -0300

    feat: add chord generation functionality and update navigation links

--- Services/MusicService.cs
+++ Services/MusicService.cs
@@ -98,6 +98,119 @@ public class MusicService
         var note = notes[_random.Next(notes.Length)];
         return note;
     }
+
+    public (string, string, int, string) GenerateRandomChord()
+    {
+        // var roots = new[] { "C", "D", "E", "F", "G", "A", "B", "_D", "_E", "_G", "_A", "_B", "^C", "^D", "^F", "^G", "^A" };
+        // var qualities = new[] { "", "m", "7", "m7", "maj7", "dim", "aug" };
+        var majorChordSteps = new[] { 0, 4, 3 };
+        var minorChordSteps = new[] { 0, 3, 4 };
+        var dominant7ChordSteps = new[] { 0, 4, 3, 3 };
+        var minor7ChordSteps = new[] { 0, 3, 4, 3 };
+        var major7ChordSteps = new[] { 0, 4, 3, 4 };
+        var diminishedChordSteps = new[] { 0, 3, 3 };
+        var majorFirstInversionSteps = new[] { 4, 3, 5 };
+        var minorFirstInversionSteps = new[] { 3, 4, 5 };
+        var majorSecondInversionSteps = new[] { 7, 5, 4 };
+        var minorSecondInversionSteps = new[] { 7, 5, 3 };
+
+        var chromaticSharp = new[] {
+            "C,", "^C,", "D,", "^D,", "E,", "F,", "^F,", "G,", "^G,", "A,", "^A,", "B,",
+            "C", "^C", "D", "^D", "E", "F", "^F", "G", "^G", "A", "^A", "B",
+            "c", "^c", "d", "^d", "e", "f", "^f", "g", "^g", "a", "^a", "b"
+        };
+
+        var chromaticFlat = new[] {
+            "C,", "_D,", "D,", "_E,", "E,", "F,", "_G,", "G,", "_A,", "A,", "_B,", "B,",
+            "C", "_D", "D", "_E", "E", "F", "_G", "G", "_A", "A", "_B", "B",
+            "c", "_d", "d", "_e", "e", "f", "_g", "g", "_a", "a", "_b", "b"
+        };
+
+        var chromatic = _random.Next(2) == 0 ? chromaticSharp : chromaticFlat;
+        int rootIndex = _random.Next(chromatic.Length - 17); // Ensure we have enough room for the chord steps
+        string root = chromatic[rootIndex].TrimEnd(',').ToUpper(); ;
+
+        if (root.StartsWith("^"))
+        {
+            root = root[1..] + "#"; // Convert to sharp notation
+        }
+        else if (root.StartsWith("_"))
+        {
+            root = root[1..] + "b"; // Convert to flat notation
+        }
+        string chordType = _random.Next(17) switch
+        {
+            <= 5 => "maj", // Major
+            <= 10 => "min", // Minor
+            <= 13 => "7",   // Dominant 7th
+            <= 14 => "m7",  // Minor 7th
+            <= 15 => "maj7",// Major 7th
+            _ => "dim", // Diminished
+        };
+
+        var inversion = 0;
+
+        if (chordType == "maj")
+        {
+            inversion = _random.Next(3);
+            switch (inversion)
+            {
+                case 0: // Root position
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, majorChordSteps));
+                case 1: // First inversion
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, majorFirstInversionSteps));
+                case 2: // Second inversion
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, majorSecondInversionSteps));
+            }
+        }
+
+        else if (chordType == "min")
+        {
+            inversion = _random.Next(3);
+            switch (inversion)
+            {
+                case 0: // Root position
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, minorChordSteps));
+                case 1: // First inversion
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, minorFirstInversionSteps));
+                case 2: // Second inversion
+                    return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, minorSecondInversionSteps));
+            }
+        }
+        else if (chordType == "7")
+        {
+            return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, dominant7ChordSteps));
+        }
+        else if (chordType == "m7")
+        {
+            return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, minor7ChordSteps));
+        }
+        else if (chordType == "maj7")
+        {
+            return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, major7ChordSteps));
+        }
+        else // Diminished
+        {
+            return (root, chordType, inversion, GenerateChordFromSteps(rootIndex, chromatic, diminishedChordSteps));
+        }
+        return ("", "", 0, "");
+    }
+
+    public string GenerateChordFromSteps(int rootIndex, string[] chromatic, int[] steps)
+    {
+        string chord = "[";
+        int noteIndex = rootIndex;
+        
+
+        foreach (var step in steps)
+        {
+            noteIndex = noteIndex + step;
+            chord += chromatic[noteIndex] + "4";
+        }
+
+        chord += "]";
+        return chord.Trim();
+    }
     
 
 }
